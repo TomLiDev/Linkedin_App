493369acd8315ba0afdf5eff6464afc1e1e51090
TomLiDev <t.litchfield00@gmail.com>
2024-11-19 20:54:30 +0000

    Add code to find ad Accounts

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os, env, requests
 
 APIKEY =  os.getenv('SECRET_KEY')
 CLIENT_ID = os.getenv('CLIENT_ID')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 
 print("test")
 
@@ -11,6 +12,27 @@ restli_client = RestliClient()
 
 response = restli_client.get(
   resource_path = "/userinfo",
-  access_token = "AQUU2C5deKhB8O48eq-T1Vj7dv2vKLcvUZCHG2PicPXeohuQAB_27Mb_8ONet8anwYIDqD3-Jew9ETgLYM7tz9oVlinvvtEzSpxPYwt30q2bAmJMUsMUNfMlFTmvLsoErMNVjTU7Wu1IUfTeT4n5vFi3XITu_ZYBy-eIla_aU46rshNO-6lHu7mdWujO25oOyjqwUbbdkmqLbe17TH7F_F7zmrm1DKvjMv2Cl1iXhafywIoYDoTl7spXQJZOKb043tuxBd7_ukPQS9DkcOtKeeQe9DFfudzA9b7osZGzqfuC60hFvEvgMqFi75fKgPPJyZ6Iek9_uOX-WgRnHkHJlXL3wMJ7bQ"
+  access_token = ACCESS_TOKEN
 )
-print(response.entity)
+#print(response.entity)
+
+response2 = restli_client.finder(
+  resource_path="/adAccounts",
+  finder_name="search",
+  query_params={
+    "search": {
+      "status": {
+        "values": ["ACTIVE", "DRAFT"]
+      },
+      "reference": {
+        "values": ["urn:li:organization:123"]
+      },
+      "test": True
+    }
+  },
+  version_string="202212",
+  access_token= ACCESS_TOKEN
+)
+ad_accounts = response2.elements
+
+print(ad_accounts)
